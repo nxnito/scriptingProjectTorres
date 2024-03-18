a44d6eb6ef8e986a3b52055de4a8fa8aac951e78
Mariana <nano091306@gmail.com>
2024-03-18 13:50:06 -0700

    GetComponent done
    finished GetComponet and get credit for it

--- P5scriptingProject/Assets/Scripts/UsingOtherComponents.cs
+++ P5scriptingProject/Assets/Scripts/UsingOtherComponents.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class UsingOtherComponents : MonoBehaviour
 {
-   public GameObject otherGameObjects
+    public GameObject otherGameObjects;
 
 
-        private AnotherScript anotherScript;
-    // Update is called once per frame
-    void Update()
+    private AnotherScript anotherScript;
+    private YetAnotherScript yetAnotherScript;
+    private BoxCollider boxCol;
+
+    void Awake()
     {
-        
+        anotherScript = GetComponent<AnotherScript>();
+        yetAnotherScript = otherGameObjects.GetComponent<YetAnotherScript>();
+        boxCol = otherGameObjects.GetComponent<BoxCollider>();
+    }
+
+    void Start()
+    {
+        boxCol.size = new Vector3(3, 3, 3);
+        Debug.Log("The player's score is " + anotherScript.playerScore);
+        Debug.Log("The player has died " + yetAnotherScript.numberOfPlayerDeaths + " times");
     }
 }
